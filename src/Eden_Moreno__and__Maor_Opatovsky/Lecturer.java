9f6d03d16bdd9d0a760e81e3e1bfa1c861ce8fa9
Th3EnD1 <eden.moreno1@gmail.com>
2026-05-19 21:05:54 +0300

    Refactored Lecturer class to use getters and setters for improved encapsulation

--- src/Eden_Moreno__and__Maor_Opatovsky/Lecturer.java
+++ src/Eden_Moreno__and__Maor_Opatovsky/Lecturer.java
@@ -13,60 +13,70 @@ public class Lecturer {
     private int committeesCount;
 
     public Lecturer(String name, eDegree degreeType, String degreeName, double salary, Department department) {
-        this.name = name;
-        this.id = ++counter;
-        this.degreeType = degreeType;
-        this.degreeName = degreeName;
-        this.salary = salary;
-        this.department = null; // No default department
-        this.committees = new Committee[2]; // Initial size
-        this.committeesCount = 0;
+        setName(name);
+        setId(++counter);
+        setDegreeType(degreeType);
+        setDegreeName(degreeName);
+        setSalary(salary);
+        setDepartment(null); // No default department
+        setCommittees(new Committee[2]); // Initial size of 2
+        setCommitteesCount(0);
     }
 
     public void addCommittee(Committee c) {
-        if (committeesCount == committees.length) {
+        if (getCommitteesCount() == getCommittees().length) {
             expandCommitteesArray();
         }
-        committees[committeesCount++] = c;
+        getCommittees()[getCommitteesCount()] = c;
+        setCommitteesCount(getCommitteesCount() + 1);
     }
 
     public void removeCommittee(Committee c) {
         int index = -1;
-        for (int i = 0; i < committeesCount; i++) {
-            if (committees[i] == c) {
+        for (int i = 0; i < getCommitteesCount(); i++) {
+            if (getCommittees()[i] == c) {
                 index = i;
                 break;
             }
         }
         if (index != -1) {
-            committees[index] = committees[committeesCount - 1]; // Override with last element
-            committees[committeesCount - 1] = null;
-            committeesCount--;
+            getCommittees()[index] = getCommittees()[getCommitteesCount() - 1]; // Override with last element
+            getCommittees()[getCommitteesCount() - 1] = null;
+            setCommitteesCount(getCommitteesCount() - 1);
         }
     }
 
     private void expandCommitteesArray() {
-        Committee[] newArr = new Committee[committees.length * 2];
-        for (int i = 0; i < committeesCount; i++) {
-            newArr[i] = committees[i];
+        Committee[] newArr = new Committee[getCommittees().length * 2];
+        for (int i = 0; i < getCommitteesCount(); i++) {
+            newArr[i] = getCommittees()[i];
         }
-        this.committees = newArr;
+        setCommittees(newArr);
     }
 
-    public int getId() { return id; }
-    public String getName() { return name; }
-    public eDegree getDegreeType() { return degreeType; }
-    public double getSalary() { return salary; }
-    public String getDegreeName() { return degreeName;};
+    public int getId() { return this.id; }
+    public void setId(int id) { this.id = id; }
+    public String getName() { return this.name; }
+    public void setName(String name) { this.name = name; }
+    public eDegree getDegreeType() { return this.degreeType; }
+    public void setDegreeType(eDegree degreeType) { this.degreeType = degreeType; }
+    public double getSalary() { return this.salary; }
+    public void setSalary(double salary) { this.salary = salary; }
+    public String getDegreeName() { return this.degreeName;};
+    public void setDegreeName(String degreeName) { this.degreeName = degreeName; }
     public void setDepartment(Department dept) { this.department = dept; }
-    public Department getDepartment() { return department; }
+    public Department getDepartment() { return this.department; }
+    public int getCommitteesCount() { return this.committeesCount; }
+    public void setCommitteesCount(int committeesCount) { this.committeesCount = committeesCount; }
+    public Committee[] getCommittees() { return this.committees; }
+    public void setCommittees(Committee[] committees) { this.committees = committees; }
 
     public String getCommitteesNames() {
-        if (committeesCount == 0) return "No committees";
+        if (getCommitteesCount() == 0) return "No committees";
         String names = "";
-        for (int i = 0; i < committeesCount; i++) {
-            names += committees[i].getName();
-            if (i < committeesCount - 1) names += ", ";
+        for (int i = 0; i < getCommitteesCount(); i++) {
+            names += getCommittees()[i].getName();
+            if (i < getCommitteesCount() - 1) names += ", ";
         }
         return names;
     }
