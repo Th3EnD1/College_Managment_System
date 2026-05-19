00dc39cad9113db7c8bc5cd8289d473fa0abac01
Th3EnD1 <eden.moreno1@gmail.com>
2026-05-19 21:25:04 +0300

    Refactored College class to use getters and setters for internal field access and commented out debug data in Main

--- src/Eden_Moreno__and__Maor_Opatovsky/College.java
+++ src/Eden_Moreno__and__Maor_Opatovsky/College.java
@@ -5,44 +5,59 @@ public class College {
     private Lecturer[] lecturers;
     private int lecturersCount;
     private Department[] departments;
+
+    public int getCommitteesCount() {return this.committeesCount;}
+    public void setCommitteesCount(int committeesCount) {this.committeesCount = committeesCount;}
+    public Committee[] getCommittees() {return this.committees;}
+    public void setCommittees(Committee[] committees) {this.committees = committees;}
+    public int getDepartmentsCount() {return this.departmentsCount;}
+    public void setDepartmentsCount(int departmentsCount) {this.departmentsCount = departmentsCount;}
+    public Department[] getDepartments() {return this.departments;}
+    public void setDepartments(Department[] departments) {this.departments = departments;}
+    public int getLecturersCount() {return this.lecturersCount;}
+    public void setLecturersCount(int lecturersCount) {this.lecturersCount = lecturersCount;}
+    public Lecturer[] getLecturers() {return this.lecturers;}
+    public void setLecturers(Lecturer[] lecturers) {this.lecturers = lecturers;}
+    public String getName() {return this.name;}
+    public void setName(String name) {this.name = name;}
+
     private int departmentsCount;
     private Committee[] committees;
     private int committeesCount;
 
     public College(String name) {
-        this.name = name;
-        this.lecturers = new Lecturer[2];
-        this.lecturersCount = 0;
-        this.departments = new Department[2];
-        this.departmentsCount = 0;
-        this.committees = new Committee[2];
-        this.committeesCount = 0;
+        setName(name);
+        setLecturers(new Lecturer[2]);
+        setCommitteesCount(0);
+        setCommittees(new Committee[2]);
+        setDepartmentsCount(0);
+        setDepartments(new Department[2]);
     }
 
     public Lecturer findLecturerById(int id) {
-        for (int i = 0; i < lecturersCount; i++) {
-            if (lecturers[i].getId() == id) return lecturers[i];
+        for (int i = 0; i < getLecturersCount(); i++) {
+            if (getLecturers()[i].getId() == id) return getLecturers()[i];
         }
         return null;
     }
 
     public Lecturer findLecturerByName(String name) {
-        for (int i = 0; i < lecturersCount; i++) {
-            if (lecturers[i].getName().equals(name)) return lecturers[i];
+        for (int i = 0; i < getLecturersCount(); i++) {
+            if (getLecturers()[i].getName().equals(name)) return getLecturers()[i];
         }
         return null;
     }
 
     public Committee findCommitteeByName(String name) {
-        for (int i = 0; i < committeesCount; i++) {
-            if (committees[i].getName().equals(name)) return committees[i];
+        for (int i = 0; i < getCommitteesCount(); i++) {
+            if (getCommittees()[i].getName().equals(name)) return getCommittees()[i];
         }
         return null;
     }
 
     public Department findDepartmentByName(String name) {
-        for (int i = 0; i < departmentsCount; i++) {
-            if (departments[i].getName().equals(name)) return departments[i];
+        for (int i = 0; i < getDepartmentsCount(); i++) {
+            if (getDepartments()[i].getName().equals(name)) return getDepartments()[i];
         }
         return null;
     }
@@ -50,8 +65,9 @@ public class College {
     // Additions to the main arrays
     public boolean addLecturer(Lecturer l) {
         if (findLecturerById(l.getId()) != null) return false;
-        if (lecturersCount == lecturers.length) expandLecturers();
-        lecturers[lecturersCount++] = l;
+        if (getLecturersCount() == getLecturers().length) expandLecturers();
+        getLecturers()[getLecturersCount()] = l;
+        setLecturersCount(getLecturersCount() + 1);
         return true;
     }
 
@@ -62,15 +78,17 @@ public class College {
         if (chairman.getDegreeType() != eDegree.PHD && chairman.getDegreeType() != eDegree.PROFESSOR) {
             return "Chairman of the committee must have a PhD or Professor degree.";
         }
-        if (committeesCount == committees.length) expandCommittees();
-        committees[committeesCount++] = new Committee(committeeName, chairman);
+        if (getCommitteesCount() == getCommittees().length) expandCommittees();
+        getCommittees()[getCommitteesCount()] = new Committee(committeeName, chairman);
+        setCommitteesCount(getCommitteesCount() + 1);
         return "Committee has been added.";
     }
 
     public boolean addDepartment(Department d) {
         if (findDepartmentByName(d.getName()) != null) return false;
-        if (departmentsCount == departments.length) expandDepartments();
-        departments[departmentsCount++] = d;
+        if (getDepartmentsCount() == getDepartments().length) expandDepartments();
+        getDepartments()[getDepartmentsCount()] = d;
+        setDepartmentsCount(getDepartmentsCount() + 1);
         return true;
     }
 
@@ -117,10 +135,10 @@ public class College {
 
     // Queries and information
     public double getCollegeAverageSalary() {
-        if (lecturersCount == 0) return 0;
+        if (getLecturersCount() == 0) return 0;
         double sum = 0;
-        for (int i = 0; i < lecturersCount; i++) sum += lecturers[i].getSalary();
-        return sum / lecturersCount;
+        for (int i = 0; i < getLecturersCount(); i++) sum += getLecturers()[i].getSalary();
+        return sum / getLecturersCount();
     }
 
     public double getDepartmentAverageSalary(String deptName) {
@@ -130,35 +148,35 @@ public class College {
     }
 
     public String getAllLecturers() {
-        if (lecturersCount == 0) return "There are no lecturers in the college.";
+        if (getLecturersCount() == 0) return "There are no lecturers in the college.";
         String res = "";
-        for (int i = 0; i < lecturersCount; i++) res += lecturers[i].toString() + "\n";
+        for (int i = 0; i < getLecturersCount(); i++) res += getLecturers()[i].toString() + "\n";
         return res;
     }
 
     public String getAllCommittees() {
-        if (committeesCount == 0) return "There are no committees in the college.";
+        if (getCommitteesCount() == 0) return "There are no committees in the college.";
         String res = "";
-        for (int i = 0; i < committeesCount; i++) res += committees[i].toString() + "\n";
+        for (int i = 0; i < getCommitteesCount(); i++) res += getCommittees()[i].toString() + "\n";
         return res;
     }
 
     // Increasing arrays by 2 times
     private void expandLecturers() {
-        Lecturer[] newArr = new Lecturer[lecturers.length * 2];
-        for (int i = 0; i < lecturersCount; i++) newArr[i] = lecturers[i];
-        this.lecturers = newArr;
+        Lecturer[] newArr = new Lecturer[getLecturers().length * 2];
+        for (int i = 0; i < getLecturersCount(); i++) newArr[i] = getLecturers()[i];
+        setLecturers(newArr);
     }
 
     private void expandDepartments() {
-        Department[] newArr = new Department[departments.length * 2];
-        for (int i = 0; i < departmentsCount; i++) newArr[i] = departments[i];
-        this.departments = newArr;
+        Department[] newArr = new Department[getDepartments().length * 2];
+        for (int i = 0; i < getDepartmentsCount(); i++) newArr[i] = getDepartments()[i];
+        setDepartments(newArr);
     }
 
     private void expandCommittees() {
-        Committee[] newArr = new Committee[committees.length * 2];
-        for (int i = 0; i < committeesCount; i++) newArr[i] = committees[i];
-        this.committees = newArr;
+        Committee[] newArr = new Committee[getCommittees().length * 2];
+        for (int i = 0; i < getCommitteesCount(); i++) newArr[i] = getCommittees()[i];
+        setCommittees(newArr);
     }
 }
