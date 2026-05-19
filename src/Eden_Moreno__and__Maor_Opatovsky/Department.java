05ee3ef1473cf601e8212258e57f25d2d15cd0e1
Th3EnD1 <eden.moreno1@gmail.com>
2026-05-19 20:46:11 +0300

    Refactored Department class to encapsulate fields using getters and setters

--- src/Eden_Moreno__and__Maor_Opatovsky/Department.java
+++ src/Eden_Moreno__and__Maor_Opatovsky/Department.java
@@ -4,74 +4,73 @@ public class Department {
     private String name;
     private int numStudents;
     private Lecturer[] lecturers;
-
-    public int getLecturersCount() {
-        return lecturersCount;
-    }
-
-    public void setLecturersCount(int lecturersCount) {
-        this.lecturersCount = lecturersCount;
-    }
-
     private int lecturersCount;
 
     public Department(String name, int numStudents) {
-        this.name = name;
-        this.numStudents = numStudents;
-        this.lecturers = new Lecturer[2];
-        this.lecturersCount = 0;
+        setName(name);
+        setNumStudents(numStudents);
+        setLecturers(new Lecturer[2]);
+        setLecturersCount(0);
     }
 
     public boolean addLecturer(Lecturer l) {
-        for (int i = 0; i < lecturersCount; i++) {
-            if (lecturers[i] == l) return false;
+        for (int i = 0; i < getLecturersCount(); i++) {
+            if (getLecturers()[i] == l) return false;
         }
-        if (lecturersCount == lecturers.length) {
+        if (getLecturersCount() == getLecturers().length) {
             expandLecturersArray();
         }
-        lecturers[lecturersCount++] = l;
+        getLecturers()[getLecturersCount()] = l;
+        setLecturersCount(getLecturersCount() + 1);
         l.setDepartment(this);
         return true;
     }
 
     public boolean removeLecturer(Lecturer l) {
         int index = -1;
-        for (int i = 0; i < lecturersCount; i++) {
-            if (lecturers[i] == l) {
+        for (int i = 0; i < getLecturersCount(); i++) {
+            if (getLecturers()[i] == l) {
                 index = i;
                 break;
             }
         }
         if (index != -1) {
             l.setDepartment(null);
-            lecturers[index] = lecturers[lecturersCount - 1];
-            lecturers[lecturersCount - 1] = null;
-            lecturersCount--;
+            getLecturers()[index] = getLecturers()[getLecturersCount() - 1];
+            getLecturers()[getLecturersCount() - 1] = null;
+            setLecturersCount(getLecturersCount() - 1);
             return true;
         }
         return false;
     }
 
     private void expandLecturersArray() {
-        Lecturer[] newArr = new Lecturer[lecturers.length * 2];
-        for (int i = 0; i < lecturersCount; i++) {
-            newArr[i] = lecturers[i];
+        Lecturer[] newArr = new Lecturer[getLecturers().length * 2];
+        for (int i = 0; i < getLecturersCount(); i++) {
+            newArr[i] = getLecturers()[i];
         }
-        this.lecturers = newArr;
+        setLecturers(newArr);
     }
 
+    public Lecturer[] getLecturers() {return lecturers;}
+    public void setLecturers(Lecturer[] lecturers) {this.lecturers = lecturers;}
+    public int getNumStudents() {return numStudents;}
+    public void setNumStudents(int numStudents) {this.numStudents = numStudents;}
     public String getName() { return name; }
+    public void setName(String name) {this.name = name;}
+    public int getLecturersCount() {return lecturersCount;}
+    public void setLecturersCount(int lecturersCount) {this.lecturersCount = lecturersCount;}
 
     public double getAverageSalary() {
-        if (lecturersCount == 0) return 0;
+        if (getLecturersCount() == 0) return 0;
         double sum = 0;
-        for (int i = 0; i < lecturersCount; i++) {
-            sum += lecturers[i].getSalary();
+        for (int i = 0; i < getLecturersCount(); i++) {
+            sum += getLecturers()[i].getSalary();
         }
-        return sum / lecturersCount;
+        return sum / getLecturersCount();
     }
 
     public String toString() {
-        return "Department: " + name + " | Number of students: " + numStudents + " | Number of lecturers: " + lecturersCount;
+        return "Department: " + getName() + " | Number of students: " + getNumStudents() + " | Number of lecturers: " + getLecturersCount();
     }
 }
