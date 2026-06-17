311c3203f27e69a7be770662315bf5e00f2659a6
Th3EnD1 <eden.moreno1@gmail.com>
2026-06-17 20:47:43 +0300

    **Created CollegeSystemException class**

---
+++ src/Eden_Moreno__and__Maor_Opatovsky/CollegeSystemException.java
@@ -0,0 +1,7 @@
+package Eden_Moreno__and__Maor_Opatovsky;
+
+public class CollegeSystemException extends Exception {
+    public CollegeSystemException(String message) {
+        super(message);
+    }
+}
