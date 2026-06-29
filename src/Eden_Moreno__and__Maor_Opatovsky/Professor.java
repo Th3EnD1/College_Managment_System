692820266e42f41ff92a8d35b80a404de17eb4f0
Th3EnD1 <eden.moreno1@gmail.com>
2026-06-29 21:52:46 +0300

    feat: implement serialization support

--- src/Eden_Moreno__and__Maor_Opatovsky/Professor.java
+++ src/Eden_Moreno__and__Maor_Opatovsky/Professor.java
@@ -1,31 +1,30 @@
 package Eden_Moreno__and__Maor_Opatovsky;
 
-public class Professor extends Doctor{
+import java.io.Serializable;
+
+public class Professor extends Doctor implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
     private String awardingBody;
 
     public Professor(String name, String degreeName, double salary, Department department, String awardingBody){
         super(name, degreeName, salary, department);
-        this.awardingBody = awardingBody;
+        setAwardingBody(awardingBody);
         setDegreeType(eDegree.PROFESSOR); // Override degree type for Professor
     }
 
-    public String getAwardingBody() {
-        return this.awardingBody;
-    }
-
-    public void setAwardingBody(String awardingBody) {
-        this.awardingBody = awardingBody;
-    }
+    public String getAwardingBody() { return this.awardingBody; }
+    public void setAwardingBody(String awardingBody) {this.awardingBody = awardingBody;}
 
     @Override
     public boolean equals(Object obj) {
         if (!super.equals(obj)) return false;
         Professor other = (Professor) obj;
-        return this.awardingBody.equals(other.awardingBody);
+        return getAwardingBody().equals(other.getAwardingBody());
     }
 
     @Override
     public String toString() {
-        return super.toString() + " | Awarding Body: " + awardingBody;
+        return super.toString() + " | Awarding Body: " + getAwardingBody();
     }
 }
